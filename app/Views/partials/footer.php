e47320a2613eeee0f09c29f17fc230e2f64f5418
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-05 21:11:02 +0900

    test

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -2,11 +2,11 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                <script>document.write(new Date().getFullYear())</script> © Skote.
+                <script>document.write(new Date().getFullYear())</script> © 2021 All right reserved
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
-                    Design & Develop by Themesbrand
+                    Design & Develop by (주)이비오
                 </div>
             </div>
         </div>
