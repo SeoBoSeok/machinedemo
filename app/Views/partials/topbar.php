b6e5cf19d9c6c892c45067cb02bf214cd70b2c4a
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-07 23:56:37 +0900

    hot fix

--- app/Views/partials/topbar.php
+++ app/Views/partials/topbar.php
@@ -191,7 +191,7 @@
                 <button type="button" class="btn header-item waves-effect" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <?php
                     $session = \Config\Services::session();
-                    print_r($session);
+                    // print_r($session);
                     $lang = $session->get('lang');
                     switch ($lang) {
                         case 'ko':
