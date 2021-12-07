026d07080bd438f1a9484d74e09f39b4214920f7
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-08 00:34:02 +0900

    hot fix

--- app/Views/form-advanced-site.php
+++ app/Views/form-advanced-site.php
@@ -35,64 +35,76 @@
 
                 <div class="row">
                     <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                        <h4 class="card-title">Example</h4>
+                                <p class="card-title-desc">This is an experimental awesome solution for responsive tables with complex data.</p>
 
-                    <table class="table mb-10">
+                                <div class="table-rep-plugin">
+                                <div class="table-responsive mb-0" data-pattern="priority-columns">
 
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Site Id</th>
-                                    <th>Description</th>
-                                    <th>Location</th>
-                                    <th>List_MachineCells</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>ST00010</td>
-                                    <td>가상공작기계 데모셀</td>
-                                    <td>한국기계연구원 14D동 107호</td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>ST00011</td>
-                                    <td>모바일 가공기 셀</td>
-                                    <td>한국기계연구원 14A동 106호</td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td>ST00000</td>
-                                    <td>전체장비</td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">4</th>
-                                    <td>ST00020</td>
-                                    <td>가상공작기계 데모셀</td>
-                                    <td>한국생산기술연구원</td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">5</th>
-                                    <td>ST00030</td>
-                                    <td>가상공작기계 실충기 전체</td>
-                                    <td>SMEC기계연, 생기원</td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">6</th>
-                                    <td>ST00100</td>
-                                    <td>연속생산 제조셀(10대 * 4대)</td>
-                                    <td></td>
-                                    <td></td>
-                                </tr>                                                                                                                                    
-                            </tbody>
-                        </table>
-                        <!-- end select2 -->
+                                <table class="table mb-10">
+
+<thead>
+    <tr>
+        <th>#</th>
+        <th>Site Id</th>
+        <th>Description</th>
+        <th>Location</th>
+        <th>List_MachineCells</th>
+    </tr>
+</thead>
+<tbody>
+    <tr>
+        <th scope="row">1</th>
+        <td>ST00010</td>
+        <td>가상공작기계 데모셀</td>
+        <td>한국기계연구원 14D동 107호</td>
+        <td></td>
+    </tr>
+    <tr>
+        <th scope="row">2</th>
+        <td>ST00011</td>
+        <td>모바일 가공기 셀</td>
+        <td>한국기계연구원 14A동 106호</td>
+        <td></td>
+    </tr>
+    <tr>
+        <th scope="row">3</th>
+        <td>ST00000</td>
+        <td>전체장비</td>
+        <td></td>
+        <td></td>
+    </tr>
+    <tr>
+        <th scope="row">4</th>
+        <td>ST00020</td>
+        <td>가상공작기계 데모셀</td>
+        <td>한국생산기술연구원</td>
+        <td></td>
+    </tr>
+    <tr>
+        <th scope="row">5</th>
+        <td>ST00030</td>
+        <td>가상공작기계 실충기 전체</td>
+        <td>SMEC기계연, 생기원</td>
+        <td></td>
+    </tr>
+    <tr>
+        <th scope="row">6</th>
+        <td>ST00100</td>
+        <td>연속생산 제조셀(10대 * 4대)</td>
+        <td></td>
+        <td></td>
+    </tr>                                                                                                                                    
+</tbody>
+</table>
+<!-- end select2 -->
+
+                                </div>
+                                </div>
+                        </div>
+                    </div>
 
                     </div>
 
