94c2a3db544e4d773dcd76827ec0d0075558c388
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-07 12:14:55 +0900

    hot fix

--- app/Views/form-advanced-cell.php
+++ app/Views/form-advanced-cell.php
@@ -41,119 +41,111 @@
                             <thead>
                                 <tr>
                                     <th>#</th>
-                                    <th>Site Id</th>
+                                    <th>Machine Cell Id</th>
                                     <th>Description</th>
-                                    <th>Location</th>
-                                    <th>List_MachineCells</th>
+                                    <th>List Objects</th>
+                                    <th>posX</th>
+                                    <th>posY</th>
+                                    <th>posZ</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
                                     <th scope="row">1</th>
-                                    <td>ST00010</td>
-                                    <td>가상공작기계 데모셀</td>
-                                    <td>한국기계연구원 14D동 107호</td>
+                                    <td>MC00010</td>
+                                    <td>SM400DH 10대</td>
                                     <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>                                    
                                 </tr>
                                 <tr>
                                     <th scope="row">2</th>
-                                    <td>ST00011</td>
-                                    <td>모바일 가공기 셀</td>
-                                    <td>한국기계연구원 14A동 106호</td>
+                                    <td>MC00010</td>
+                                    <td>SM400DH 10대</td>
                                     <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>                                    
                                 </tr>
                                 <tr>
                                     <th scope="row">3</th>
-                                    <td>ST00000</td>
-                                    <td>전체장비</td>
+                                    <td>MC00010</td>
+                                    <td>SM400DH 10대</td>
                                     <td></td>
                                     <td></td>
+                                    <td></td>
+                                    <td></td>                                    
                                 </tr>
                                 <tr>
                                     <th scope="row">4</th>
-                                    <td>ST00020</td>
-                                    <td>가상공작기계 데모셀</td>
-                                    <td>한국생산기술연구원</td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">5</th>
-                                    <td>ST00030</td>
-                                    <td>가상공작기계 실충기 전체</td>
-                                    <td>SMEC기계연, 생기원</td>
-                                    <td></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">6</th>
-                                    <td>ST00100</td>
-                                    <td>연속생산 제조셀(10대 * 4대)</td>
+                                    <td>MC00010</td>
+                                    <td>SM400DH 10대</td>
                                     <td></td>
                                     <td></td>
-                                </tr>                                                                                                                                    
+                                    <td></td>
+                                    <td></td>                                    
+                                </tr>                                                                                                                                 
                             </tbody>
                         </table>
+                        <!-- end select2 -->
 
-                        <div class="card">
+                    </div>
+
+                    <div class="col-lg-6">
+                    <div class="card">
                             <div class="card-body">
 
-                                <h4 class="card-title">Site 새등록</h4>
-                                <!-- <p class="card-title-desc">Site 등록</p> -->
+                                <h4 class="card-title">Machine 새등록</h4>
+                                <!-- <p class="card-title-desc">Machine 등록</p> -->
 
                                 <form>
                                     <div class="row">
-                                        <div class="col-lg-6">
+                                        <div>
                                             <div class="mb-3 row">
-                                                <label for="example-text-input" class="col-md-2 col-form-label">Site Id</label>
+                                                <label for="example-text-input" class="col-md-2 col-form-label">Machine Id</label>
                                                 <div class="col-md-10">
-                                                    <input class="form-control" type="text" value="Artisanal kale" id="example-text-input">
-                                                </div>
-                                            </div>
-                                            <div class="mb-3 row">
-                                                <label for="example-text-input" class="col-md-2 col-form-label">Location</label>
-                                                <div class="col-md-10">
-                                                    <input class="form-control" type="text" value="Artisanal kale" id="example-text-input">
+                                                    <input class="form-control" type="text" value="" id="example-text-input">
                                                 </div>
                                             </div>
                                             <div class="mb-3 row">
                                                 <label class="form-label">List of Machine Cells</label>
 
                                                 <select class="select2 form-control select2-multiple" multiple="multiple" data-placeholder="Choose ...">
-                                                    <optgroup label="Alaskan/Hawaiian Time Zone">
-                                                        <option value="AK">MC00010</option>
-                                                        <option value="HI">MC00011</option>
+                                                    <optgroup label="한국기계연구원 14D동">
+                                                        <option value="AK">MT04301</option>
+                                                        <option value="HI">MT04302</option>
                                                     </optgroup>
-                                                    <optgroup label="Pacific Time Zone">
-                                                        <option value="CA">MA0001</option>
-                                                        <option value="NV">MA0002</option>
-                                                        <option value="OR">MA0003</option>
+                                                    <optgroup label="한국기계연구원 14A동">
+                                                        <option value="CA">MT03011</option>
+                                                        <option value="NV">MT03012</option>
+                                                        <option value="OR">MT03013</option>
                                                     </optgroup>
                                                 </select>
 
-                                            </div>     
-                                        </div>
-
-                                        <div class="col-lg-6">
+                                            </div> 
                                             <div class="">
                                                 <label class="mb-1">Description</label>
                                                 <p class="text-muted mb-2">
                                                     
                                                 </p>
-                                                <textarea id="textarea" class="form-control" maxlength="225" rows="4" placeholder="This textarea has a limit of 225 chars."></textarea>
+                                                <textarea id="textarea" class="form-control" maxlength="225" rows="2" placeholder="This textarea has a limit of 225 chars."></textarea>
                                             </div>                                       
                                             <button class="btn btn-light dropdown-toggle w-100 mt-2" type="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                                 <i class="mdi mdi-plus me-1"></i> Create New
-                                            </button>
+                                            </button>                                                
                                         </div>
+
+                                        <!-- <div class="col-lg-6">
+                                            
+                                        </div> -->
                                     </div>
 
                                 </form>
 
                             </div>
-                        </div>
-                        <!-- end select2 -->
-
+                        </div>                        
                     </div>
-
 
                 </div>
                 <!-- end row -->
