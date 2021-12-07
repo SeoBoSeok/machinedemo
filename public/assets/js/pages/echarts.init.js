7f3fc208ee6869daf4108ae649297201992d2994
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-07 15:20:00 +0900

    hot fix

--- public/assets/js/pages/echarts.init.js
+++ public/assets/js/pages/echarts.init.js
@@ -674,104 +674,104 @@ if (option3 && typeof option2 === "object") {
 
 // Machine MSC
 
-var dom = document.getElementById("bar-chart-machine");
-var myChart = echarts.init(dom);
-var app = {};
-optionMSC = null;
+// var dom = document.getElementById("bar-chart-machine");
+// var myChart = echarts.init(dom);
+// var app = {};
+// optionMSC = null;
 
-optionMSC = {
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        // Use axis to trigger tooltip
-        type: 'shadow' // 'shadow' as default; can also be 'line' or 'shadow'
-      }
-    },
-    legend: {},
-    grid: {
-      left: '3%',
-      right: '4%',
-      bottom: '3%',
-      containLabel: true
-    },
-    xAxis: {
-      type: 'value'
-    },
-    yAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    },
-    series: [
-      {
-        name: 'Direct',
-        type: 'bar',
-        stack: 'total',
-        label: {
-          show: true
-        },
-        emphasis: {
-          focus: 'series'
-        },
-        data: [320, 302, 301, 334, 390, 330, 320]
-      },
-      {
-        name: 'Mail Ad',
-        type: 'bar',
-        stack: 'total',
-        label: {
-          show: true
-        },
-        emphasis: {
-          focus: 'series'
-        },
-        data: [120, 132, 101, 134, 90, 230, 210]
-      },
-      {
-        name: 'Affiliate Ad',
-        type: 'bar',
-        stack: 'total',
-        label: {
-          show: true
-        },
-        emphasis: {
-          focus: 'series'
-        },
-        data: [220, 182, 191, 234, 290, 330, 310]
-      },
-      {
-        name: 'Video Ad',
-        type: 'bar',
-        stack: 'total',
-        label: {
-          show: true
-        },
-        emphasis: {
-          focus: 'series'
-        },
-        data: [150, 212, 201, 154, 190, 330, 410]
-      },
-      {
-        name: 'Search Engine',
-        type: 'bar',
-        stack: 'total',
-        label: {
-          show: true
-        },
-        emphasis: {
-          focus: 'series'
-        },
-        data: [820, 832, 901, 934, 1290, 1330, 1320]
-      }
-    ]
-  };
+// optionMSC = {
+//     tooltip: {
+//       trigger: 'axis',
+//       axisPointer: {
+//         // Use axis to trigger tooltip
+//         type: 'shadow' // 'shadow' as default; can also be 'line' or 'shadow'
+//       }
+//     },
+//     legend: {},
+//     grid: {
+//       left: '3%',
+//       right: '4%',
+//       bottom: '3%',
+//       containLabel: true
+//     },
+//     xAxis: {
+//       type: 'value'
+//     },
+//     yAxis: {
+//       type: 'category',
+//       data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+//     },
+//     series: [
+//       {
+//         name: 'Direct',
+//         type: 'bar',
+//         stack: 'total',
+//         label: {
+//           show: true
+//         },
+//         emphasis: {
+//           focus: 'series'
+//         },
+//         data: [320, 302, 301, 334, 390, 330, 320]
+//       },
+//       {
+//         name: 'Mail Ad',
+//         type: 'bar',
+//         stack: 'total',
+//         label: {
+//           show: true
+//         },
+//         emphasis: {
+//           focus: 'series'
+//         },
+//         data: [120, 132, 101, 134, 90, 230, 210]
+//       },
+//       {
+//         name: 'Affiliate Ad',
+//         type: 'bar',
+//         stack: 'total',
+//         label: {
+//           show: true
+//         },
+//         emphasis: {
+//           focus: 'series'
+//         },
+//         data: [220, 182, 191, 234, 290, 330, 310]
+//       },
+//       {
+//         name: 'Video Ad',
+//         type: 'bar',
+//         stack: 'total',
+//         label: {
+//           show: true
+//         },
+//         emphasis: {
+//           focus: 'series'
+//         },
+//         data: [150, 212, 201, 154, 190, 330, 410]
+//       },
+//       {
+//         name: 'Search Engine',
+//         type: 'bar',
+//         stack: 'total',
+//         label: {
+//           show: true
+//         },
+//         emphasis: {
+//           focus: 'series'
+//         },
+//         data: [820, 832, 901, 934, 1290, 1330, 1320]
+//       }
+//     ]
+//   };
 
-setInterval(function () {
-    optionMSC.series[0].data[0].value = Math.round((Math.random() * 2).toFixed(2)) + 30;
-    myChart.setOption(optionMSC, true);
-},1000);
+// setInterval(function () {
+//     optionMSC.series[0].data[0].value = Math.round((Math.random() * 2).toFixed(2)) + 30;
+//     myChart.setOption(optionMSC, true);
+// },1000);
 
-if (optionMSC && typeof optionMSC === "object") {
-    myChart.setOption(optionMSC, true);
-}
+// if (optionMSC && typeof optionMSC === "object") {
+//     myChart.setOption(optionMSC, true);
+// }
 
-optionMSC && myChart.setOption(optionMSC);
+// optionMSC && myChart.setOption(optionMSC);
