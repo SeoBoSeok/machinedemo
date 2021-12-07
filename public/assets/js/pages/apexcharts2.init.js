199fb31342b4173e369ab7ee4f82b60e32aa2272
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-07 16:58:28 +0900

    hot fix

--- public/assets/js/pages/apexcharts2.init.js
+++ public/assets/js/pages/apexcharts2.init.js
@@ -5,7 +5,20 @@ Website: https://themesbrand.com/
 Contact: themesbrand@gmail.com
 File: Apex Chart init js
 */
+function generateDayWiseTimeSeries(baseval, count, yrange) {
+  var i = 0;
+  var series = [];
+  while (i < count) {
+    var x = baseval;
+    var y =
+      Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
 
+    series.push([x, y]);
+    baseval += 86400000;
+    i++;
+  }
+  return series;
+}
 //  line chart datalabel
    
 var options = {
