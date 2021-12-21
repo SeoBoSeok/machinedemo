fb92a529cdabd101a9a35e20d8cad549a43809e1
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-22 08:55:15 +0900

    hot fix

--- app/Views/pages-qa-details.php
+++ app/Views/pages-qa-details.php
@@ -97,7 +97,7 @@
     </style>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
     <?php
-      print_r($title_meta);
+      // print_r($title_meta);
     ?>
 <header>
   <div class="collapse bg-dark" id="navbarHeader">
