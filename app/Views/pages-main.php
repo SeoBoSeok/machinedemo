8979e23bc8ccfc7692a2e109e882608e101394b4
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-21 11:41:08 +0900

    hot fix

--- app/Views/pages-main.php
+++ app/Views/pages-main.php
@@ -74,6 +74,7 @@
     .card-body {background-color: white !important; padding: 3rem !important; color: #808080;}
     .main_button {background-color: #03A9F4 !important; border: 0 !important; font-size: 2rem;}
     .add_h2 {padding-bottom: 30px; color: #000!important;}
+    .card {border: 1px solid #E3E3E3; border-radius: 5px;}
     </style>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
     
@@ -118,12 +119,13 @@
   <section class="py-5 text-center container">
     <div class="row py-lg-5">
       <div class="col-lg-6 col-md-8 mx-auto">
-        <h1 class="fw-light">Album example</h1>
-        <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-        <p>
+          <h4>가상머신기반 지능화 S/W</h4>
+        <h1 class="fw-light">VIRTUAL MACHINE SYSTEM</h1>
+        <h1 class="fw-light">가상공작기계</h1>
+        <!-- <p>
           <a href="#" class="btn btn-primary my-2">Main call to action</a>
           <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-        </p>
+        </p> -->
       </div>
     </div>
   </section>
