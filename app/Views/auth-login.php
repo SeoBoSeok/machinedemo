659491fca3c541275a51358b4493d6de4a2365c3
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-21 12:55:52 +0900

    hot fix

--- app/Views/auth-login.php
+++ app/Views/auth-login.php
@@ -48,7 +48,7 @@
                                 <a href="/" class="auth-logo-dark">
                                     <div class="avatar-md profile-user-wid mb-4">
                                         <span class="avatar-title rounded-circle bg-light">
-                                            <img src="assets/images/logo.svg" alt="" class="rounded-circle" height="34">
+                                            <img src="assets/images/kimm_logo.png" alt="" class="rounded-circle" height="34">
                                         </span>
                                     </div>
                                 </a>
