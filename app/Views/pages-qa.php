132b300ad92c035fab49d1fb066435df61be2433
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-21 13:32:04 +0900

    hot fix

--- app/Views/pages-qa.php
+++ app/Views/pages-qa.php
@@ -157,25 +157,7 @@
       <div class="row">
 
       <div class="card">
-      <div class="checkout-tabs">
-                            <div class="row">
-                                <div class="col-lg-2">
-                                    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                                        <a class="nav-link active" id="v-pills-gen-ques-tab" data-bs-toggle="pill" href="#v-pills-gen-ques" role="tab" aria-controls="v-pills-gen-ques" aria-selected="true">
-                                            <i class="bx bx-question-mark d-block check-nav-icon mt-4 mb-2"></i>
-                                            <p class="fw-bold mb-4">General Questions</p>
-                                        </a>
-                                        <a class="nav-link" id="v-pills-privacy-tab" data-bs-toggle="pill" href="#v-pills-privacy" role="tab" aria-controls="v-pills-privacy" aria-selected="false"> 
-                                            <i class="bx bx-check-shield d-block check-nav-icon mt-4 mb-2"></i>
-                                            <p class="fw-bold mb-4">Privacy Policy</p>
-                                        </a>
-                                        <a class="nav-link" id="v-pills-support-tab" data-bs-toggle="pill" href="#v-pills-support" role="tab" aria-controls="v-pills-support" aria-selected="false">
-                                            <i class="bx bx-support d-block check-nav-icon mt-4 mb-2"></i>
-                                            <p class="fw-bold mb-4">Support</p>
-                                        </a>
-                                    </div>
-                                </div>
-                                <div class="col-lg-10">
+      <div class="col">
                                     <div class="card">
                                         <div class="card-body">
                                             <div class="tab-content" id="v-pills-tabContent">
@@ -331,8 +313,6 @@
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
       </div>
 
       <div class="card">
