a7baf6fb81b330a50224905744b9647d39709510
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-21 13:28:24 +0900

    hot fix

--- app/Views/pages-qa.php
+++ app/Views/pages-qa.php
@@ -125,8 +125,8 @@
       <ul class="nav nav-pills">
         <li class="nav-item"><a href="#" class="nav-link" aria-current="page">Product</a></li>
         <li class="nav-item"><a href="#" class="nav-link">Manual</a></li>
-        <li class="nav-item on"><a href="#" class="nav-link">Download</a></li>
-        <li class="nav-item"><a href="pages/faq" class="nav-link">Q&A</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Download</a></li>
+        <li class="nav-item on"><a href="pages/faq" class="nav-link">Q&A</a></li>
         <li class="nav-item"><a href="/auth-login" class="nav-link">Sign in</a></li>
       </ul>
     </div>
