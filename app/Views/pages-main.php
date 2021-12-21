fceb78af1eb1bd45ee49aefa2ad856a9f785cf77
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-21 10:28:28 +0900

    hot fix

--- app/Views/pages-main.php
+++ app/Views/pages-main.php
@@ -59,6 +59,13 @@
     .navbar>.container a, .navbar>.container strong {
         color: black;
     }
+    .nav-pills {list-style: none;}
+    .nav-pills::after {
+        display: table;
+        content: "";
+        clear: both;
+    }
+    .nav-pills li {float: left;}
     </style>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
     
