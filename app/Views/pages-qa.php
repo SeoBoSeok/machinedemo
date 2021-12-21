e10f451588f4ed69f8088f4a1e104aa72d56a47d
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-21 13:34:52 +0900

    hot fix

--- app/Views/pages-qa.php
+++ app/Views/pages-qa.php
@@ -159,14 +159,14 @@
                                         <div class="card-body">
                                             <div class="tab-content" id="v-pills-tabContent">
                                                 <div class="tab-pane fade active show" id="v-pills-gen-ques" role="tabpanel" aria-labelledby="v-pills-gen-ques-tab">
-                                                    <h4 class="card-title mb-5">General Questions</h4>
+                                                    <h4 class="card-title mb-5">자주 묻는 질문</h4>
                                                     <div class="faq-box d-flex mb-4">
                                                         <div class="flex-shrink-0 me-3 faq-icon">
                                                             <i class="bx bx-help-circle font-size-20 text-success"></i>
                                                         </div>
                                                         <div class="flex-grow-1">
-                                                            <h5 class="font-size-15">What is Lorem Ipsum?</h5>
-                                                            <p class="text-muted">New common language will be more simple and regular than the existing European languages. It will be as simple as occidental.</p>
+                                                            <h5 class="font-size-15">FocasAPI 의 C# BackgroundWorker 스레드 상에서 비정상 작동에 관한 질문</h5>
+                                                            <!-- <p class="text-muted"></p> -->
                                                         </div>
                                                     </div>
                                                     <div class="faq-box d-flex mb-4">
@@ -174,8 +174,8 @@
                                                             <i class="bx bx-help-circle font-size-20 text-success"></i>
                                                         </div>
                                                         <div class="flex-grow-1">
-                                                            <h5 class="font-size-15">Where does it come from?</h5>
-                                                            <p class="text-muted">Everyone realizes why a new common language would be desirable one could refuse to pay expensive translators.</p>
+                                                            <h5 class="font-size-15">FocasApi.cnc_rdtofsr 에 관한 문의 1 file</h5>
+                                                            <!-- <p class="text-muted">Everyone realizes why a new common language would be desirable one could refuse to pay expensive translators.</p> -->
                                                         </div>
                                                     </div>
                                                     <div class="faq-box d-flex mb-4">
@@ -183,8 +183,8 @@
                                                             <i class="bx bx-help-circle font-size-20 text-success"></i>
                                                         </div>
                                                         <div class="flex-grow-1">
-                                                            <h5 class="font-size-15">Where can I get some?</h5>
-                                                            <p class="text-muted">If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages.</p>
+                                                            <h5 class="font-size-15">FocasApi.cnc_rdparam 관련 문의 2 file</h5>
+                                                            <!-- <p class="text-muted">If several languages coalesce, the grammar of the resulting language is more simple and regular than that of the individual languages.</p> -->
                                                         </div>
                                                     </div>
                                                     <div class="faq-box d-flex mb-4">
@@ -192,17 +192,8 @@
                                                             <i class="bx bx-help-circle font-size-20 text-success"></i>
                                                         </div>
                                                         <div class="flex-grow-1">
-                                                            <h5 class="font-size-15">Why do we use it?</h5>
-                                                            <p class="text-muted">Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary.</p>
-                                                        </div>
-                                                    </div>
-                                                    <div class="faq-box d-flex">
-                                                        <div class="flex-shrink-0 me-3 faq-icon">
-                                                            <i class="bx bx-help-circle font-size-20 text-success"></i>
-                                                        </div>
-                                                        <div class="flex-grow-1">
-                                                            <h5 class="font-size-15">Where can I get some?</h5>
-                                                            <p class="text-muted">To an English person, it will seem like simplified English, as a skeptical Cambridge friend of mine told me what Occidental</p>
+                                                            <h5 class="font-size-15">메뉴얼 질문드립니다. 1</h5>
+                                                            <!-- <p class="text-muted">Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary.</p> -->
                                                         </div>
                                                     </div>
                                                 </div>
