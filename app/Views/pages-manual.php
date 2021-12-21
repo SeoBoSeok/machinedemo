e9968a11629ccd6a8f9d54d7483db6c8085ae83b
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-21 12:40:32 +0900

    hot fix

--- app/Views/pages-manual.php
+++ app/Views/pages-manual.php
@@ -93,7 +93,7 @@
     .container.video {padding: 30px 200px;background-color: rgb(248, 249, 250);}
     .video-container { position: relative; padding-bottom: 56.25%; padding-top: 30px; height: 0; overflow: hidden; } .video-container iframe, .video-container object, .video-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }
     .sub_heading_txt {padding-top: 20px;}
-    .manual_list a {min-width: 270px; text-align: center; color: #000;}
+    .manual_list a {min-width: 270px; text-align: center; color: #000!important;; display: inline-block!important;}
     </style>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
     
