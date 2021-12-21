511ddd3a144175444007c92671beb9fc9fa994dd
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-21 10:23:44 +0900

    hot fix

--- app/Views/pages-main.php
+++ app/Views/pages-main.php
@@ -84,9 +84,14 @@
         <!-- <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="me-2" viewBox="0 0 24 24"><path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"></path><circle cx="12" cy="13" r="4"></circle></svg> -->
         <strong><span class="high">VMS 가상</span>공작기계</strong>
       </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+      <ul className="nav nav-pills">
+        <li className="nav-item"><a href="#" className="nav-link" aria-current="page">Product</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Manuel</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">Download</a></li>
+        <li className="nav-item"><a href="pages/faq" className="nav-link">FAQs</a></li>
+        <li className="nav-item"><a href="#" className="nav-link">About</a></li>
+        <li className="nav-item"><a href="/login" className="nav-link">Login</a></li>
+      </ul>
     </div>
   </div>
 </header>
