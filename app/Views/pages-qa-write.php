7d900061567368d204622bae58e808689c616d04
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-23 07:45:54 +0900

    hot fix

--- app/Views/pages-qa-write.php
+++ app/Views/pages-qa-write.php
@@ -187,7 +187,7 @@
                                     <div class="card">
                                         <div class="card-body">
                                             
-                                        <form>
+                                        <form id="update_form">
                                     <div class="row mb-4">
                                         <label for="horizontal-firstname-input" class="col-sm-3 col-form-label">제 목</label>
                                         <div class="col-sm-9">
@@ -223,7 +223,28 @@
       </div>
 
   </div>
-
+<script>
+  $('#update_form').submit(function(e){
+    e.preventDefault();
+    $.ajax({
+      url:'pages-qa-update' // 요청 할 주소
+      async:true,// false 일 경우 동기 요청으로 변경
+      type:'POST' // GET, PUT
+      data: {
+          form_title:'ajax',
+          Age:'10'
+      },// 전송할 데이터
+      dataType:'json',// xml, json, script, html
+      beforeSend:function(jqXHR) {},// 서버 요청 전 호출 되는 함수 return false; 일 경우 요청 중단
+      success:function(jqXHR) {
+        alert('글이 등록되었습니다');
+        window.location.href = '/pages-qa';
+      },// 요청 완료 시
+      error:function(jqXHR) {},// 요청 실패.
+      complete:function(jqXHR) {}// 요청의 실패, 성공과 상관 없이 완료 될 경우 호출
+    });
+  });
+</script>
 </main>
 
 </body>
