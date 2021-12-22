7b8308dae0ab5cbcd19252e70b70f204d8f69609
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-23 08:04:41 +0900

    hot fix

--- app/Controllers/PageController.php
+++ app/Controllers/PageController.php
@@ -177,7 +177,6 @@ class PageController extends BaseController
 	public function show_pages_qa_update(){
 		$something = isset($_POST['form_title']) ? $_POST['form_title'] : null;
 		echo $something;
-		echo '1111';
 	}		
 
 	public function show_pages_comingsoon(){
