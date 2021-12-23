b8fe155e850d04df102a54cb4adab0505f817bc7
SeoBoSeok <ggy7bbo@gmail.com>
2021-12-23 12:44:02 +0900

    hot fix

--- app/Views/pages-starter.php
+++ app/Views/pages-starter.php
@@ -29,7 +29,7 @@
                     </div> <!-- container-fluid -->
                 </div>
                 <!-- End Page-content -->
-
+                <button id="test" type="button">test</button>
                 
                 <?= $this->include('partials/footer') ?>
             </div>
@@ -43,6 +43,24 @@
         <?= $this->include('partials/vendor-scripts') ?>
 
         <script src="assets/js/app.js"></script>
-
+        <script type="text/javascript" src="http://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script>
+            $('#test').click(function(){
+                $.ajax({
+                    url:'localhost:3000/users', // 요청 할 주소
+                    type:'POST', // GET, PUT
+                    data: {
+                        form_title:'ajax',
+                        Age:'10'
+                    },// 전송할 데이터
+                    dataType:'json',// xml, json, script, html
+                    success:function(jqXHR) {
+                        console.log(jqXHR);
+                    },// 요청 완료 시
+                    error:function(jqXHR) {},// 요청 실패.
+                    complete:function(jqXHR) {}// 요청의 실패, 성공과 상관 없이 완료 될 경우 호출
+                    });
+            });
+        </script>
     </body>
 </html>
